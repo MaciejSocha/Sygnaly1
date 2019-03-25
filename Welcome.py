84cf1fe0310d0edb78338ae6eed643a51ec22deb
Florke07 <maciek.socha1@gmail.com>
2019-03-25 20:27:06 +0100

    more tests

--- Welcome.py
+++ Welcome.py
@@ -10,14 +10,19 @@ print("Hello")
 
 sig = SygnalyCiagle.SinusoidalnyWyprostowanyDwupolowkowo(1, 20, 0.0, 20)
 
+sig2 = SygnalyCiagle.SinusoidalnyWyprostowanyDwupolowkowo(0.5, 5, 0.0, 20)
+
 xTab = []
 yTab = []
 x = 0
+yTab2 = []
 
 while x < 50:
     xTab.append(x)
     yTab.append(sig.x(x))
+    yTab2.append(sig2.x(x))
     x += 0.1
 
 plt.plot(xTab, yTab)
+plt.plot(xTab, yTab2)
 plt.show()
