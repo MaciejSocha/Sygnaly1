dfa24af361c56a9710f14956eeead5ed616e264d
Wredter <wiktor.muraszko@wp.pl>
2019-06-04 12:33:23 +0200

    Dobra teraz działa

--- Part3/NewerApp.py
+++ Part3/NewerApp.py
@@ -1,3 +1,11 @@
-
+from Part3 import Splot
 
 print("Test")
+tabX1 = [0, 1, 2, 3]
+tabY1 = [1, 2, 3, 4]
+tabX2 = [0, 1, 2]
+tabY2 = [5, 6, 7]
+
+x, y = Splot.SplotDyskretny(tabX1, tabY1, tabX2, tabY2)
+print(x)
+print(y)
--- Part3/Splot.py
+++ Part3/Splot.py
@@ -1,21 +1,21 @@
 
-def SplotDyskretny(tabX1,tabY1,tabX2,tabY2):
+def SplotDyskretny(tabX1, tabY1, tabX2, tabY2):
     yTab = []
     xTab = []
     i = 0
     M = len(tabY1)
     N = len(tabY2)
-    if tabX1 != tabX2:
+    if tabX1[1] != tabX2[1]:
         print("UWAGA SPRAWDŹ OKRESY SYGNAŁÓW")
     for x in range(M + N - 1):
         sum = 0
         j = 0
         for val in tabY1:
-            if i-j < len(tabY2):
+            if i-j < len(tabY2) and i-j >= 0:
                 sum += val*tabY2[i-j]
             j += 1
-        xTab[i] = i * tabX1
-        yTab[i] = sum
+        xTab.append(i * tabX1[1])
+        yTab.append(sum)
         i += 1
 
     return xTab, yTab
