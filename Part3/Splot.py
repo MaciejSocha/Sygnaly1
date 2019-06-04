e0dd03f3dac0b610d32ae0810848897fca97e192
Wredter <wiktor.muraszko@wp.pl>
2019-06-04 12:03:15 +0200

    Splot teraz powinno być ok

--- Part3/Splot.py
+++ Part3/Splot.py
@@ -1,11 +1,22 @@
 
 def SplotDyskretny(tabX1,tabY1,tabX2,tabY2):
-    xTab = []
     yTab = []
-    if len(tabX1) == len(tabX2):
-        i = 0
-        for val in reversed(tabY2):
-            xTab[i] = tabX1
-            yTab[i] = tabY1[i]+val
+    xTab = []
+    i = 0
+    M = len(tabY1)
+    N = len(tabY2)
+    if tabX1 != tabX2:
+        print("UWAGA SPRAWDŹ OKRESY SYGNAŁÓW")
+    for x in range(M + N - 1):
+        sum = 0
+        j = 0
+        for val in tabY1:
+            if i-j < len(tabY2):
+                sum += val*tabY2[i-j]
+            j += 1
+        xTab[i] = i * tabX1
+        yTab[i] = sum
+        i += 1
+
     return xTab, yTab
 
